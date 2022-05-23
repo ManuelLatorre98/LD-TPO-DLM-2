57867726983edf6645167d9c714b4bf1350d66d4
Santino Castagno <santinocastagno.dev@gmail.com>
2022-05-23 19:57:25 -0300

    class updated
    Co-authored-by: manulatorre98@hotmail.com <manulatorre98@hotmail.com>
    Co-authored-by: limonada01 <limonada01@users.noreply.github.com>

--- site/src/index.erl
+++ site/src/index.erl
@@ -1,39 +1,44 @@
 %% -*- mode: nitrogen -*-
--module (index).
+-module(index).
 -compile(export_all).
 -include_lib("nitrogen_core/include/wf.hrl").
 
-main() -> #template { file="./site/templates/bare.html" }.
+main() -> #template{file = "./site/templates/bare.html"}.
 
 title() -> "Welcome to Nitrogen".
 
 body() ->
-    #container_12 { body=[
-        #grid_8 { alpha=true, prefix=2, suffix=2, omega=true, body=inner_body() }
-    ]}.
+    #container_12{
+        body = [
+            #grid_8{alpha = true, prefix = 2, suffix = 2, omega = true, body = inner_body()}
+        ]
+    }.
 
-inner_body() -> 
+inner_body() ->
     [
-        #h1 { text="Welcome to Nitrogen" },
+        #h1{class = header, text = "Welcome to Nitrogen"},
+        #h1{style = "color:red;", text = "Welcome to Nitrogen"},
         #p{},
-        "
-        If you can see this page, then your Nitrogen server is up and
-        running. Click the button below to test postbacks.
-        ",
-        #p{}, 	
-        #button { id=button, text="Click me!", postback=click },
-		#p{},
-        "
-        Run <b>./bin/dev help</b> to see some useful developer commands.
-        ",
-		#p{},
-		"
-		<b>Want to see the ",#link{text="Sample Nitrogen jQuery Mobile Page",url="/mobile"},"?</b>
-		"
+        "\n"
+        "        If you can see this page, then your Nitrogen server is up and\n"
+        "        running. Click the button below to test postbacks probando.\n"
+        "        ",
+        #p{},
+        #button{id = button, text = "Click me!", postback = click},
+        #p{},
+        "\n"
+        "        Run <b>./bin/dev help</b> to see some useful developer commands.\n"
+        "        ",
+        #p{},
+        "\n"
+        "		<b>Want to see the ",
+        #link{text = "Sample Nitrogen jQuery Mobile Page", url = "/mobile"},
+        "?</b>\n"
+        "		"
     ].
-	
+
 event(click) ->
-    wf:replace(button, #panel { 
-        body="You clicked the button!", 
-        actions=#effect { effect=highlight }
+    wf:replace(button, #panel{
+        body = "You clicked the button!",
+        actions = #effect{effect = highlight}
     }).
