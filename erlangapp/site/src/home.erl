812c46f448dfafcc2ac4d8bbfbad15b776490d2f
limonada01 <daniel.campos@est.fi.uncoma.edu.ar>
2022-06-08 20:28:25 -0300

    dias de primavera

--- erlangapp/site/src/home.erl
+++ erlangapp/site/src/home.erl
@@ -15,21 +15,32 @@ body() ->
     
 
 left() ->
+    UsuariosConectados= messenger:get_user_list(),
     #panel {class='usuariosConectados' ,body=[
-         #label{class='userName', text="Usuarios Conectados:"}   
-
+         #label{class='userName', text="Usuarios Conectados:"},  
+         #list{
+            class='listaUsuarios',  
+            body=[#listitem{body=Usuario}|| Usuario <- UsuariosConectados]}
     ]}.
 
 mid() -> 
     CurrentUser =  wf:user() ,
+    ListaChat= messenger:get_historial_chat(),
     #panel {class='mensaje',body=[
-        
-     
         #label{class='userName', text=CurrentUser},
-        #textbox { id=messageTextBox, class='cajaTexto', next=sendButton },
-        #button { id=sendButton, text="Enviar", postback={chat} }
-
-        
+        #panel{class='chatPanel',body=[
+            #list{
+            class='chat',  
+            body=[#listitem{body=[[Nombre],": ",[Mensaje]]}|| {Nombre,Mensaje} <- ListaChat]}
+        ]},
+        #panel{class='sendMensaje',body=[
+            #textbox { id=messageTextBox, class='cajaTexto', next=sendButton },
+            #button { id=sendButton, text="Enviar", postback=chat }
+        ]}
     ]}.
 
+event(chat) ->
+        Mensaje = wf:q(messageTextBox),
+        %?PRINT("HOLA"). %messenger:server(user_list),
+        messenger:message_all(Mensaje).
 
