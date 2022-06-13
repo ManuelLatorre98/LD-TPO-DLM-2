3881e13a8aa37df4b0f2107864b607b8cd54edb2
ManuelLatorre <manuel.latorre@est.fi.uncoma.edu.ar>
2022-06-13 17:14:04 -0300

    front

--- erlangapp/site/src/home.erl
+++ erlangapp/site/src/home.erl
@@ -16,25 +16,39 @@ body() ->
 
 left() ->
     UsuariosConectados= messenger:get_user_list(),
-    #panel {class='usuariosConectados' ,body=[
-         #label{class='userName', text="Usuarios Conectados:"},  
-         #list{
-            class='listaUsuarios',  
-            body=[#listitem{body=Usuario}|| Usuario <- UsuariosConectados]}
+    
+    #panel {class='leftContainer' ,body=[
+        #label{class='connectedUsersTitle', text="Usuarios Conectados:"},
+        #panel{class='connectedUsersContainer', body=[
+            #list{
+                class='listaUsuarios',  
+                body=[#listitem{class= 'userCard', body=Usuario}|| Usuario <- UsuariosConectados]
+            }]
+        }
+         
     ]}.
 
 mid() -> 
-    CurrentUser =  wf:user() ,
+    CurrentUser =  wf:user() , 
     ListaChat= messenger:get_historial_chat(),
-    #panel {class='mensaje',body=[
-        #panel{class='mainHeader', body=[
-            #label{class='namePage', text="Messenger"},
-            #label{class='userName', text=CurrentUser}
-        ]},
+    #panel {class='messagesContainer',body=[
+        % #panel{class='chatHeader', body=[
+        %     #label{class='namePage', text="Messenger"},
+        %     #label{class='userName', text=CurrentUser}
+        % ]},
         #panel{class='chatPanel',body=[
             #list{
             class='chat',  
-            body=[#listitem{body=[[Nombre],": ",[Mensaje]]}|| {Nombre,Mensaje} <- ListaChat]}
+            body=[#listitem{class='messageList', body=[
+                #panel{class='messageCardContainer', body=[
+                    #p{class='messageCard_UserInfo', text= [Nombre,":"]},
+                    #p{class='messageCard_message', text=[Mensaje]}
+                ]}
+                %[Nombre],": ",[Mensaje]
+                ]
+            
+            
+            }|| {Nombre,Mensaje} <- ListaChat]}
         ]},
         #panel{class='sendMensaje',body=[
             #textbox { id=messageTextBox, class='cajaTexto', next=sendButton },
